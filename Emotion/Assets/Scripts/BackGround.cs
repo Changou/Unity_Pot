40cc0100acb80a02120a1b0583dcbcc9e2c17589
Changou <117820115+Changou@users.noreply.github.com>
2024-07-02 00:46:39 +0900

    Emotion 프로젝트 시작
    메인메뉴 생성

---
+++ Emotion/Assets/Scripts/BackGround.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    [Header("¼Óµµ")]
+    [SerializeField] float speed;
+
+    [Header("ÁÂÇ¥")]
+    [SerializeField] float posValue;
+
+    Vector2 startPos;
+    float newPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newPos = Mathf.Repeat(Time.time * speed, posValue);
+        transform.position = startPos + Vector2.left * newPos;
+    }
+}
